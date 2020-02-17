f176ca3f5bc3e7166300b569e74443e301d2c39d
Metainy <Mahmoud@Live.com>
2020-02-17 02:23:59 +0200

    Project Structure

---
+++ Core/Config/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Database information
+ */
+define("DATABASE_TYPE", "mysql");
+define("DATABASE_HOST", "localhost");
+define("DATABASE_PORT", "3306");
+define("DATABASE_NAME", "shop_db");
+define("DATABASE_USERNAME", "root");
+define("DATABASE_PASSWORD", "");
