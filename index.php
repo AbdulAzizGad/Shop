f02edbe9e4930677c74fe399e0b442292feea431
Metainy <Mahmoud@Live.com>
2020-02-17 03:34:40 +0200

    Adding Bootstrap

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Head -->
+<?php include_once "views/parts/head.php"; ?>
+
+<body>
+<div class="alert alert-primary" role="alert">
+    A simple primary alert—check it out!
+</div>
+
+    <!-- Footer -->
+    <?php include_once "views/parts/footer.php"; ?>
+
+</body>
+</html>
